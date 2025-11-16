b0a40bf1f4d206cd4f5d6d5e4105d7a6dff1057e
Ittipat Pattum <nampu@finema.co>
2025-11-16 11:51:10 +0700

    save

--- app/components/top/NavDesktop.vue
+++ app/components/top/NavDesktop.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="flex items-center px-[120px] h-[120px] bg-green-500">
-        <div class="text-[32px] text-black font-itgb pb-2 mr-16">SHOP.CO</div>
+        <div class="text-[32px] text-black font-itgb pb-2 mr-16"><NuxtLink to="/">SHOP.CO</NuxtLink></div>
         <button type="button" class="cursor-pointer flex justify-center items-center mr-10" @click="debugClicked('closed!!')">
             <div class="text-[16px] font-stv">Shop</div>
             <div class="pt-2.5"><Icon name="ri:arrow-down-s-line" style="color: black" size="32px" /></div>
@@ -14,8 +14,9 @@
             <input
                 type="text"
                 placeholder="Search for products..."
-                class="w-full bg-search-grey text-[16px] font-stv h-12 rounded-full pl-10 pr-4 focus:outline-none placeholder:text-blk-op-40 placeholder:text-[16px] placeholder:font-stv"
+                class="w-full bg-search-grey text-[16px] font-stv h-12 rounded-full pl-14 pr-10 focus:outline-none placeholder:text-blk-op-40 placeholder:text-[16px] placeholder:font-stv"
             />
+            <div class="absolute left-4 top-2.5"><Icon name="ri:search-line" style="color: var(--color-blk-op-40)" size="28px" /></div>
         </div>
         <button type="button" class="cursor-pointer flex justify-center items-center" @click="debugClicked('closed!!')"><Icon name="ri:shopping-cart-2-line" style="color: black" size="32px" /></button>
         <button type="button" class="cursor-pointer flex justify-center items-center ml-4" @click="debugClicked('closed!!')"><Icon name="ri:account-circle-line" style="color: black" size="32px" /></button>
--- app/components/top/NavMobile.vue
+++ app/components/top/NavMobile.vue
@@ -3,7 +3,7 @@
 <template>
     <div class="flex items-center px-4 h-[72px] bg-amber-500">
         <button type="button" class="cursor-pointer flex justify-center items-center" @click="debugClicked('closed!!')"><Icon name="ri:menu-line" style="color: black" size="32px" /></button>
-        <div class="text-[26px] text-black font-itgb flex-1 pb-1.5 ml-4">SHOP.CO</div>
+        <div class="text-[26px] text-black font-itgb flex-1 pb-1.5 ml-4"><NuxtLink to="/">SHOP.CO</NuxtLink></div>
         <button type="button" class="cursor-pointer flex justify-center items-center ml-4" @click="debugClicked('closed!!')"><Icon name="ri:search-line" style="color: black" size="32px" /></button>
         <button type="button" class="cursor-pointer flex justify-center items-center ml-4" @click="debugClicked('closed!!')"><Icon name="ri:shopping-cart-2-line" style="color: black" size="32px" /></button>
         <button type="button" class="cursor-pointer flex justify-center items-center ml-4" @click="debugClicked('closed!!')"><Icon name="ri:account-circle-line" style="color: black" size="32px" /></button>
