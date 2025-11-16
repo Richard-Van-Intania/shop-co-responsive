d2f353f187ca0af5144547d8d4b2cf224caac6b3
Ittipat Pattum <nampu@finema.co>
2025-11-16 11:59:34 +0700

    save

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,12 +4,12 @@
     <div>
         <div class="fixed left-0 top-0 right-0 z-10">
             <TopBanner />
-            <div class="block"><TopNavMobile /></div>
-            <div class="block"><TopNavDesktop /></div>
+            <div class="block xl:hidden"><TopNavMobile /></div>
+            <div class="hidden xl:block"><TopNavDesktop /></div>
         </div>
         <div class="h-[38px]" />
-        <div class="h-[72px]" />
-        <div class="h-[120px]" />
+        <div class="h-[72px] block xl:hidden" />
+        <div class="h-[120px] hidden xl:block" />
         <slot />
         <BottomFooter />
     </div>
