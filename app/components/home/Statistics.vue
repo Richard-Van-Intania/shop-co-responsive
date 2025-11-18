7824674f74cf615e019adf9196bcc9c66deabf86
Ittipat Pattum <nampu@finema.co>
2025-11-18 23:12:08 +0700

    save

---
+++ app/components/home/Statistics.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+    label: { type: String, required: true },
+    numeral: { type: Number, required: true },
+});
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <div class="font-stv font-bold leading-tight text-[24px] md:text-[32px] xl:text-[40px]">{{ formatNumber(props.numeral) }}+</div>
+        <div class="font-stv text-blk-op-60 text-[12px] md:text-[14px] xl:text-[16px]">{{ props.label }}</div>
+    </div>
+</template>
+
+<style scoped></style>
--- app/components/home/WelcomeCardMobile.vue
+++ app/components/home/WelcomeCardMobile.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="flex flex-col">
-        <div class="px-4">FIND CLOTHES THAT MATCHES YOUR STYLE</div>
-        <div class="px-4">Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</div>
+    <div class="flex flex-col bg-top-grey">
+        <div class="px-4 pt-8 text-[36px] text-black font-itgb leading-none">FIND CLOTHES THAT MATCHES YOUR STYLE</div>
+        <div class="px-4 pt-6 text-[14px] font-stv text-blk-op-60">Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</div>
+        <div class="px-4 pt-6"><button type="button" class="bg-black w-full h-14 rounded-full text-[16px] font-stv font-medium text-white cursor-pointer" v-on:click="debugClicked('Shop Now')">Shop Now</button></div>
+        <div class="flex"><HomeStatistics label="International Brands" v-bind:numeral="200" /></div>
     </div>
 </template>
 
